b414f7a1de6118ef8f59ced54aa168a2bf602856
ilyasokur <47240755+ilyasokur@users.noreply.github.com>
2025-04-07 01:26:34 +0300

    hot fix

--- hotel-service/src/main/java/com/example/hotel_service/controller/HotelController.java
+++ hotel-service/src/main/java/com/example/hotel_service/controller/HotelController.java
@@ -1,17 +1,18 @@
 package com.example.hotel_service.controller;
 
 import com.example.hotel_service.dto.HotelCreateRequest;
+import com.example.hotel_service.model.Hotel;
 import com.example.hotel_service.repository.HotelRepository;
 import com.example.hotel_service.service.HotelService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.mapping.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/create-hotel")
+@RequestMapping("/api/hotel")
 @RequiredArgsConstructor
 public class HotelController {
-    private final HotelRepository hotelRepository;
     private final HotelService hotelService;
 
     @PostMapping
@@ -20,4 +21,5 @@ public class HotelController {
         hotelService.CreateHotel(hotelCreateRequest);
         return "Hotel created";
     }
+
 }
