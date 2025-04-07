4f23b424387199cbe9902405f18706bc5a9402a4
ivanz-thinkpad <Ivan.zakharov.ru@yandex.ru>
2025-04-07 21:33:48 +0300

    add getAllHotels

--- hotel-service/src/main/java/com/example/hotel_service/controller/HotelController.java
+++ hotel-service/src/main/java/com/example/hotel_service/controller/HotelController.java
@@ -1,11 +1,12 @@
 package com.example.hotel_service.controller;
 
 import com.example.hotel_service.dto.HotelCreateRequest;
+import com.example.hotel_service.dto.HotelResponse;
 import com.example.hotel_service.model.Hotel;
 import com.example.hotel_service.repository.HotelRepository;
 import com.example.hotel_service.service.HotelService;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.mapping.List;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,4 +23,9 @@ public class HotelController {
         return "Hotel created";
     }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<HotelResponse> getAllHotels() {
+        return hotelService.getAllHotels();
+    }
 }
