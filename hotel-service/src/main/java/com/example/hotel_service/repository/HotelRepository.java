0e31ea157ad38979df13007a707c0b0fb9cd4831
ivanz-thinkpad <Ivan.zakharov.ru@yandex.ru>
2025-04-20 22:45:56 +0300

    add pagination

--- hotel-service/src/main/java/com/example/hotel_service/repository/HotelRepository.java
+++ hotel-service/src/main/java/com/example/hotel_service/repository/HotelRepository.java
@@ -1,7 +1,14 @@
 package com.example.hotel_service.repository;
 
+import com.example.hotel_service.dto.HotelResponse;
 import com.example.hotel_service.model.Hotel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
+
 }
