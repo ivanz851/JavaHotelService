bfea1e7dbc14688016af4ca9a198bd9b20fdeb6d
Valerii Vartanov <valera.v.2003@gmail.com>
2025-05-20 00:03:02 +0300

    add price from db

--- hotel-service/src/main/java/com/example/hotel_service/service/GrpcServiceHotel.java
+++ hotel-service/src/main/java/com/example/hotel_service/service/GrpcServiceHotel.java
@@ -4,12 +4,23 @@ import com.example.hotel.grpc.CheckRoomAvailabilityRequest;
 import com.example.hotel.grpc.CheckRoomAvailabilityResponse;
 import com.example.hotel.grpc.HotelServiceGrpc;
 
+import com.example.hotel_service.model.Room;
+import com.example.hotel_service.repository.RoomRepository;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 
 @GrpcService
 public class GrpcServiceHotel extends HotelServiceGrpc.HotelServiceImplBase {
+
+
+    private final RoomRepository roomRepository;
+
+    public GrpcServiceHotel(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
     @Override
     public void roomPrice(CheckRoomAvailabilityRequest request,
                           StreamObserver<CheckRoomAvailabilityResponse> responseObserver) {
@@ -17,18 +28,25 @@ public class GrpcServiceHotel extends HotelServiceGrpc.HotelServiceImplBase {
         long hotelId = request.getHotelId();
         long roomId = request.getRoomId();
 
-        float price = calculatePrice(hotelId, roomId);
+        try {
+            float price = calculatePrice(hotelId, roomId);
 
-        CheckRoomAvailabilityResponse response = CheckRoomAvailabilityResponse.newBuilder()
-                .setPrice(price)
-                .build();
+            CheckRoomAvailabilityResponse response = CheckRoomAvailabilityResponse.newBuilder()
+                    .setPrice(price)
+                    .build();
 
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+        } catch (Exception e) {
+            responseObserver.onError(Status.NOT_FOUND.withDescription("Room not found").asException());
+        }
     }
 
     private float calculatePrice(long hotelId, long roomId) {
-        return 100.0f + (hotelId % 10) * 10 + (roomId % 5) * 5;
+        Room room = roomRepository.findById(roomId).
+                orElseThrow(() -> new IllegalArgumentException("Room not found"));
+        return room.getPricePerNight().floatValue();
     }
 
 }
